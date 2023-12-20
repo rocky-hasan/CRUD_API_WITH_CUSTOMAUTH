a5281bee235c037da1a09080775b48549c286113
rocky-hasan <rakibulhasancblock1a@gmail.com>
2023-12-20 17:49:13 +0600

    complete API

--- crud/views.py
+++ crud/views.py
@@ -1,48 +1,82 @@
 from django.shortcuts import render
 from rest_framework.parsers import JSONParser
 from crud.serializers import StudentSerializers
-from rest_framework.renderers import JSONRenderer
-from django.views.decorators.csrf import csrf_exempt
 from crud.models import Student
 from django.http import JsonResponse, HttpResponse
 from rest_framework import status
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import generics, mixins
 
 # Create your views here.
-@api_view(['GET', 'POST'])
-def student_api_list(request):
-    if request.method=='GET':
-        stu=Student.objects.all()
-        serializers=StudentSerializers(stu,many=True)
-        return Response(serializers.data)        
-    elif request.method == 'POST':
-        serializer = StudentSerializers(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#class Student_api_list(mixins.ListModelMixin,mixins.CreateModelMixin): or use (generics.ListCreateAPIView)
+class Student_api_list(generics.ListCreateAPIView):
+    queryset=Student.objects.all()
+    serializer_class = StudentSerializers
+
+    def get(self, request):
+        return self.list(request)
+
+    def post(self, request):
+        return self.create(request)
+
+# class Student_detail(generics.RetrieveUpdateDestroyAPIView) if use this dont need mixins
+class Student_detail(mixins.RetrieveModelMixin,mixins.UpdateModelMixin,mixins.DestroyModelMixin,generics.GenericAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializers
+
+    def get(self, request,*args,**kwargs):
+        return self.retrieve(request,*args,**kwargs)
+
+    def put(self, request,*args,**kwargs):
+        return self.update(request,*args,**kwargs )
+
+    def delete(self, request,*args,**kwargs):
+        return self.destroy(request,*args,**kwargs)
+
+
+
+
+
+
+
+
+
+# class Student_api_list(APIView):
+#     def get(self,request,format=None):
+#         stu=Student.objects.all()
+#         serializers=StudentSerializers(stu,many=True)
+#         return Response(serializers.data)        
+#     def post(self,request,format=None):
+#         serializer = StudentSerializers(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-@api_view(['GET', 'PUT', 'DELETE'])
-def student_api(request, pk):
-    try:
-        stu = Student.objects.get(pk=pk)
-    except Student.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
 
-    if request.method == 'GET':
-        serializer = StudentSerializers(stu)
-        return Response(serializer.data)
+# class Student_detail(APIView):
+#     def get_objects(self,pk):
+#         try:
+#             stu = Student.objects.get(pk=pk)
+#         except Student.DoesNotExist:
+#             return Response(status=status.HTTP_404_NOT_FOUND)
 
-    elif request.method == 'PUT':           # For  UPDATE
-        serializer = StudentSerializers(stu, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     def get(self, request, pk, format=None):
+#         serializer = self.get_object(pk)
+#         return Response(serializer.data)
 
-    elif request.method == 'DELETE':   # For Delete
-        stu.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+#     def put(self, request, pk, format=None):          # For  UPDATE
+#         stu = self.get_objects(pk)
+#         serializer=StudentSerializers(stu,request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#     def delete(self,request,pk,format=None):  # For Delete
+#         stu=self.get_objects(pk)
+#         stu.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
--- crud_api/urls.py
+++ crud_api/urls.py
@@ -2,9 +2,11 @@
 from django.contrib import admin
 from django.urls import path
 from crud import views
+from crud import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('student_api_list/',views.student_api_list),
-    path('studentapi/<int:pk>/',views.student_api),
+    path('student_api_list/',views.Student_api_list.as_view()),
+    path('student_api_list/<int:pk>/',views.Student_detail.as_view()),
 ]
